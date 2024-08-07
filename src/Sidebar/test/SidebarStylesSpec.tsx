3b908d54e1bdd3366d2f4fc50ad4fd3f856286cb
Christian Caracach <christiancaracach@gmail.com>
2024-08-07 17:47:08 -0300

    test(Sidebar): removing use of getDOMNode

--- src/Sidebar/test/SidebarStylesSpec.tsx
+++ src/Sidebar/test/SidebarStylesSpec.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Sidebar from '../index';
-import { getDOMNode, getStyle, itChrome } from '@test/utils';
+import { getStyle, itChrome } from '@test/utils';
 
 import '../styles/index.less';
 
 describe('Sidebar styles', () => {
   itChrome('Should render the correct styles', () => {
     const instanceRef = React.createRef<HTMLDivElement>();
-    render(<Sidebar ref={instanceRef} className="rs-sidebar-collapse" />);
-    const dom = getDOMNode(instanceRef.current);
-    assert.equal(
-      getStyle(dom, 'transition'),
-      'flex 0.15s ease-in 0s, width 0.15s ease-in 0s',
-      'Sidebar collapse transition'
-    );
+    const { container } = render(<Sidebar ref={instanceRef} className="rs-sidebar-collapse" />);
+
+    console.log(getStyle(container.firstChild as Element, 'transition')),
+      expect(getStyle(container.firstChild as Element, 'transition')).to.be.equal(
+        'flex 0.15s ease-in, width 0.15s ease-in'
+      );
   });
 });
