648cf13bf829899c9fe13f7e6b669ffa45e4adf8
Christian Caracach <christiancaracach@gmail.com>
2024-11-17 19:54:26 -0300

    refactor(Content): removing getDOMNode and using expect

--- src/Content/test/ContentStylesSpec.tsx
+++ src/Content/test/ContentStylesSpec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Content from '../index';
-import { getDOMNode, getStyle, itChrome } from '@test/utils';
+import { itChrome } from '@test/utils';
 
 import '../styles/index.less';
 
@@ -9,6 +9,7 @@ describe('Content styles', () => {
   itChrome('Should render the correct styles', () => {
     const instanceRef = React.createRef<HTMLDivElement>();
     render(<Content ref={instanceRef}>Title</Content>);
-    assert.equal(getStyle(getDOMNode(instanceRef.current), 'flex'), '1 1 auto');
+    const dom = instanceRef.current as Element;
+    expect(dom).to.have.style('flex', '1 1 auto');
   });
 });
